b76d7f3b516b58f956cfe21f2017b4b2e2f39a0f
Ewan Stott <ewan.stott1@gmail.com>
2023-12-11 11:45:56 +0000

    commit

--- entry.js
+++ entry.js
@@ -1,15 +1,15 @@
-const rootRef = document.getElementById("root");
+// const rootRef = document.getElementById("root");
 
-async function getApiData() {
-  const result1 = await axios.get(
-    `https://thesimpsonsquoteapi.glitch.me/quotes?character=homer simpson`
-  );
-  const result2 = await axios.get(
-    `https://thesimpsonsquoteapi.glitch.me/quotes?character=bart simpson`
-  );
-  const result3 = await axios.get(
-    `https://thesimpsonsquoteapi.glitch.me/quotes?character=lisa simpson`
-  );
-  console.log(result1, result2, result3);
-}
-getApiData();
+// async function getApiData() {
+//   const result1 = await axios.get(
+//     `https://thesimpsonsquoteapi.glitch.me/quotes?character=homer simpson`
+//   );
+//   const result2 = await axios.get(
+//     `https://thesimpsonsquoteapi.glitch.me/quotes?character=bart simpson`
+//   );
+//   const result3 = await axios.get(
+//     `https://thesimpsonsquoteapi.glitch.me/quotes?character=lisa simpson`
+//   );
+//   console.log(result1, result2, result3);
+// }
+// getApiData();
--- promises_&_async.js
+++ promises_&_async.js
@@ -27,26 +27,67 @@
 
 //An enhancement on promises:
 //Async/await (wrapping promise in a function so reusable) - "Un-then-ing"
-function genPromise() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("It worked");
-    }, 1000);
-  });
-}
+// function genPromise() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("It worked");
+//     }, 1000);
+//   });
+// }
 
-async function go() {
-  try {
-    const result1 = await genPromise();
-    const result2 = await genPromise();
-    const result3 = await genPromise();
-    const result4 = await genPromise();
-    const result5 = await genPromise();
+// async function go() {
+//   try {
+//     const result1 = await genPromise();
+//     const result2 = await genPromise();
+//     const result3 = await genPromise();
+//     const result4 = await genPromise();
+//     const result5 = await genPromise();
 
-    console.log(result1);
-  } catch (err) {
-    console.log(err);
-  }
-}
-go();
+//     console.log(result1);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// go();
 //would NOT normally write promises (usually done automatically^^)
+
+// JavaScript Promises In 10 Minutes Eample (https://www.youtube.com/watch?v=DHvZLI7Db8E)
+
+// let p = new Promise((resolve, reject) => {
+//   let a = 1 + 2;
+//   if (a == 2) {
+//     resolve("success");
+//   } else {
+//     reject("fail");
+//   }
+// });
+
+// //then called if promise successful
+// p.then((message) => {
+//   console.log("This is in the then " + message);
+//   //catch called if promise rejected or fails
+// }).catch((message) => {
+//   console.log("This is in the catch " + message);
+// });
+
+// //Normal function
+// function sum(a, b) {
+//   return a + b;
+// }
+// //arrow function
+// const sum2 = (a, b) => a + b;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("it did not work");
+  }, 2000);
+});
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+console.log(promise);
